6d91ce75e643525e99fce43ecd123a1783112e94
Yousuf Ali <yousuf.pharma@gmail.com>
2022-11-24 01:58:35 -0500

    golem

--- R/app_server.R
+++ R/app_server.R
@@ -6,4 +6,25 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+  	js_data <- shiny::reactive({
+		json_data <- my_dataset
+		to_json <- jsonlite::toJSON(json_data)
+		# print(to_json)
+		to_json
+	})
+
+	shiny::observe({
+		session$sendCustomMessage("json_data", js_data())
+	})
+
+    shiny::observe({
+        req(input$down_button)
+        session$sendCustomMessage("down_tree_png", input$down_button)
+    })
+    shiny::observe({
+        req(input$down_button_svg)
+        session$sendCustomMessage("down_tree_svg", input$down_button_svg)
+    })
 }
+
+
--- R/app_ui.R
+++ R/app_ui.R
@@ -8,10 +8,28 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
+
+ htmlTemplate("inst/app/www/template.html",
+    button = actionButton("down_button", "Download PNG"),
+    button_svg = actionButton("down_button_svg", "Download SVG"),
+    slider = sliderInput("x", "X", 1, 100, 50),
+    # d3 = htmltools::includeScript("inst/app/www/index.js"),
+    # shiny_app = htmltools::includeScript("inst/app/www/shiny_app.js"),
+    # dnd = htmltools::includeScript("www/dndTree.js"),
+    # d3_context = htmltools::includeScript("www/d3-context-menu.js"),
+    # under_score = htmltools::includeScript("www/underscore-min.js"),
+    # jq = htmltools::includeScript("www/jquery.js"),
+    # fast_click = htmltools::includeScript("www/fastclick.js"),
+    # foundation = htmltools::includeScript("www/foundation.min.js"),
+    # download_svg = htmltools::includeScript("www/svgDownload.js"),
+    # file_saver = htmltools::includeScript("www/FileSaver.js"),
+    # canvas_js = htmltools::includeScript("www/canvas-to-blob.js"),
+    # d3_css = htmltools::includeCSS("www/d3-context-menu.css"),
+    # foundation_css = htmltools::includeCSS("www/foundation.css"),
+    # app_css = htmltools::includeCSS("www/app.css"),
+)
     # Your application UI logic
-    fluidPage(
-      h1("tree")
-    )
+  
   )
 }
 
@@ -26,15 +44,19 @@ app_ui <- function(request) {
 golem_add_external_resources <- function() {
   add_resource_path(
     "www",
-    app_sys("app/www")
+    system.file("app/www", package = "tree")
   )
 
   tags$head(
-    favicon(),
-    bundle_resources(
-      path = app_sys("app/www"),
-      app_title = "tree"
-    )
+    # favicon(),
+    # bundle_resources(
+    #   path = app_sys("app/www"),
+    #   app_title = "tree"
+    # ),
+    tags$script(src = "www/index.js"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "www/app.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "www/d3-context-menu.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "www/foundation.css"),
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
   )
