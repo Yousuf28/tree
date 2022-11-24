0a94f9faa1b665fd7e4a5ae66e9783ab64c5eb1e
Yousuf Ali <yousuf.pharma@gmail.com>
2022-11-24 01:07:52 -0500

    file added

--- srcjs/index.js
+++ srcjs/index.js
@@ -1,5 +1,18 @@
 import { message } from './modules/message.js';
 import 'shiny';
+import './modules/canvas-to-blob.js';
+import './modules/d3.v3.min.js';
+import './modules/d3-context-menu.js';
+import './modules/dndTree.js';
+import './modules/fastclick.js';
+import './modules/FileSaver.js';
+import './modules/foundation.min.js';
+import './modules/shiny_app.js';
+import './modules/svgDownload.js';
+import './modules/underscore-min.js';
+// import './modules/app.css';
+// import './modules/d3-context-menu.css';
+// import './modules/foundation.css';
 
 // In shiny server use:
 // session$sendCustomMessage('show-packer', 'hello packer!')
